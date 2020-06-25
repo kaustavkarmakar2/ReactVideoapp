f67b72fa61dccd38abd511510b7b4b4d5efdc5b4
kaustavkarmakar2 <kaustavkarmakar2@gmail.com>
2020-06-26 00:19:33 +0530

    somechanges

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ io.on('connection', socket => {
     })
 });
 
-server.listen(3000, () => console.log('server is running on port 8000'));
+server.listen(8000, () => console.log('server is running on port 8000'));
 
 
